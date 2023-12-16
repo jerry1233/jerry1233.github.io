483e8311dd07f59308a85a912a31547f01f3aadb
unknown <jerrymaster@protonmail.com>
2023-12-16 22:09:31 +0800

    firest commit

--- upload_to_github.bat
+++ upload_to_github.bat
@@ -1,7 +1,8 @@
 git config --global http.proxy http://127.0.0.1:8118
 git add .
 git commit -m "firest commit"
-git remote add origin git@github.com:jerry1233/jerry1233.github.io.git
+
+git remote set-url origin https://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@github.com/jerry1233/jerry1233.github.io.git
 git push -u origin main
 git config --global --unset http.proxy
 pause
